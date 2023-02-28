0197f9ce0870cca1602aebb72ace63563c3fe0a4
Arun Kumar <112400317+arunkumar080591@users.noreply.github.com>
2023-02-28 20:48:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let counter = document.querySelector(".counter");
+let followers = document.querySelector(".followers");
+
+let count = 1;
+
+setInterval(() => {
+    if(count < 1000)
+    {
+        count++;
+        counter.innerText = count;
+    }
+} , 1);
+
+setTimeout(() => {
+    followers.innerText = "Followers in Instagram";
+}, 5200);
